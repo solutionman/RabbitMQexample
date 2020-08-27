2d38499c8a0deaf78048d47ff24e476eaa21f07f
lom <lom@ssvet.info>
2020-08-27 10:02:02 +0700

    connection url

--- app/src/main/java/com/example/rabbitmqexample/MainActivity.java
+++ app/src/main/java/com/example/rabbitmqexample/MainActivity.java
@@ -134,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
     ConnectionFactory factory = new ConnectionFactory();
     private void setupConnectionFactory() {
 //        String uri = "CLOUDAMQP_URL";
-        String uri = "amqp://guest:guest@localhost";
+        String uri = "amqp://guest:guest@192.168.220.87:5672";
         try {
             factory.setAutomaticRecoveryEnabled(false);
             factory.setUri(uri);
